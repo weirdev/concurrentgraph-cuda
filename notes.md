5f90e4ae43742a79900ebc8f3f63650da5dc47b0
Wes Weirather <weirdev@outlook.com>
2019-06-10 17:41:26 -0500

    Hello world

---
+++ notes.md
@@ -0,0 +1,2 @@
+https://stackoverflow.com/questions/16438099/high-level-gpu-programming-in-c
+
---
+++ test1/main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <cuda.h>
+
+int main(void) {
+    printf("Hello world!\n");
+    return 0;
+}
