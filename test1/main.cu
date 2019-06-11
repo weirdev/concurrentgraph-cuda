facc96322302445148ad3d2e2b8e8ceba68b69c3
Wes Weirather <weirdev@outlook.com>
2019-06-11 02:08:08 -0500

    Commented out dllexport

--- test1/main.cu
+++ test1/main.cu
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cuda.h>
 
+/*
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -10,6 +11,7 @@ void __declspec(dllexport) negative_prob_multiply_matrix_vector(float* matrix, f
 #ifdef __cplusplus
 }
 #endif
+*/
 
 int main(void) {
     printf("Hello world!\n");
@@ -19,7 +21,7 @@ int main(void) {
 void multiply_matrix_vector(float* matrix, float* in_vector, float* out_vector, int* outerdim, int* innerdim) {
     for (int i=0; i < outerdim; i++) {
         float sum = 0;
-        for (int j=0; j < innerdim, j++) {
+        for (int j=0; j < innerdim; j++) {
             sum += matrix[i*innerdim + j] * in_vector[j];
         }
         out_vector[i] = sum;
@@ -29,7 +31,7 @@ void multiply_matrix_vector(float* matrix, float* in_vector, float* out_vector, 
 void negative_prob_multiply_matrix_vector(float* matrix, float* in_vector, float* out_vector, uint outerdim, uint innerdim) {
     for (int i=0; i < outerdim; i++) {
         float prob = 1;
-        for (int j=0; j < innerdim, j++) {
+        for (int j=0; j < innerdim; j++) {
             prob *= 1 - (matrix[i*innerdim + j] * in_vector[j]);
         }
         out_vector[i] = prob;
