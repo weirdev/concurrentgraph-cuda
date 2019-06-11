548f45cd904b3cb62e9c6ea2ad37d0947c2af936
Wes Weirather <weirdev@outlook.com>
2019-06-11 01:56:46 -0500

    Export negative_prob_multiply_matrix_vector() as library function.

--- test1/main.cu
+++ test1/main.cu
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <cuda.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void __declspec(dllexport) negative_prob_multiply_matrix_vector(float* matrix, float* in_vector, float* out_vector, uint outerdim, uint innerdim);
+
+#ifdef __cplusplus
+}
+#endif
+
 int main(void) {
     printf("Hello world!\n");
     return 0;
+}
+
+void multiply_matrix_vector(float* matrix, float* in_vector, float* out_vector, int* outerdim, int* innerdim) {
+    for (int i=0; i < outerdim; i++) {
+        float sum = 0;
+        for (int j=0; j < innerdim, j++) {
+            sum += matrix[i*innerdim + j] * in_vector[j];
+        }
+        out_vector[i] = sum;
+    }
+}
+
+void negative_prob_multiply_matrix_vector(float* matrix, float* in_vector, float* out_vector, uint outerdim, uint innerdim) {
+    for (int i=0; i < outerdim; i++) {
+        float prob = 1;
+        for (int j=0; j < innerdim, j++) {
+            prob *= 1 - (matrix[i*innerdim + j] * in_vector[j]);
+        }
+        out_vector[i] = prob;
+    }
 }
